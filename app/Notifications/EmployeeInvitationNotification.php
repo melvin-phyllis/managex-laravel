b048d3f47698db0493fb60981e14271221d07478
melvin-phyllis <melvinphyllisakou@gmail.com>
2026-02-12 15:53:30 +0000

    update

--- app/Notifications/EmployeeInvitationNotification.php
+++ app/Notifications/EmployeeInvitationNotification.php
@@ -3,15 +3,11 @@
 namespace App\Notifications;
 
 use App\Models\EmployeeInvitation;
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class EmployeeInvitationNotification extends Notification implements ShouldQueue
+class EmployeeInvitationNotification extends Notification
 {
-    use Queueable;
-
     public function __construct(
         public EmployeeInvitation $invitation
     ) {}
